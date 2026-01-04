a96d5fe7a83eb2c150521d3df4f9ad00a5389192
KCastanho <kevincastanho.pro@gmail.com>
2026-01-04 05:11:05 +0100

    prepare express API for vercel

--- config/db_mongo.js
+++ config/db_mongo.js
@@ -1,9 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectDB = (mongoURI, dbName) => {
-    mongoose.connect(mongoURI, {dbName: dbName})
-    .then(() => console.log('connexion à mongo reussi !'))
-    .catch(error => console.log(`Erreur de connexion à mongo : ${error}`))
-}
+let cached = global.mongoose;
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
+
+const connectDB = async (mongoUri) => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(mongoUri);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
 
 module.exports = connectDB;
